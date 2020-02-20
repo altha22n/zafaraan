2e15cd2566d69847fa971628c032f6d9eaed8248
Nada <altha22n@mtholyoke.edu>
2020-02-19 23:34:46 -0500

    Updates

---
+++ precache-manifest.450f2ce4eb4fb259cd8fcb3881f31313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917048429d1cce2d21caaf71ecdd6c86",
+    "url": "/zafaraan/index.html"
+  },
+  {
+    "revision": "48ba8260819d2cfe9b88",
+    "url": "/zafaraan/static/css/main.aa1ea87e.chunk.css"
+  },
+  {
+    "revision": "78c7cc0353df4fa4c7b1",
+    "url": "/zafaraan/static/js/2.23fd2380.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/zafaraan/static/js/2.23fd2380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ba8260819d2cfe9b88",
+    "url": "/zafaraan/static/js/main.a8012fee.chunk.js"
+  },
+  {
+    "revision": "fbbe6d9f8291c9aef88c",
+    "url": "/zafaraan/static/js/runtime-main.149fc837.js"
+  },
+  {
+    "revision": "a1d6e0914d390a4b64efd109006a4d0b",
+    "url": "/zafaraan/static/media/background.a1d6e091.jpg"
+  }
+]);
